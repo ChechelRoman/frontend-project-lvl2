3ee74d4c98ec973299b754b18fc1e3902e72bea6
Chechel Roman <chechel-roman@lenta.ru>
2021-05-17 18:08:23 +0700

    fix to gendiff help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,11 @@
-import gendiff from '../index.js';
+#!/usr/bin/env node
 
-gendiff.parse();
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+program.parse();
